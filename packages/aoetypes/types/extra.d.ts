1d9584d19fe3ceffc1be07bf3deea416728c4363
Robin Kahlow <tora@warlock.ai>
2022-04-11 19:07:24 +0100

    ad dPLAYERS and TEAM_NEUTRAL globals, 0.1.7

--- packages/aoetypes/types/extra.d.ts
+++ packages/aoetypes/types/extra.d.ts
@@ -5,3 +5,13 @@
  * @param path Path to import.
  */
 declare function importScar(path: string): void;
+
+/**
+ * List of all players.
+ */
+declare const PLAYERS: Player[];
+
+/**
+ * Neutral team id
+ */
+declare const TEAM_NEUTRAL: number;
